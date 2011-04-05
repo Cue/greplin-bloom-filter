88c6d0f0dd4fab76c3ef906a55735509492b17e0
Shaneal Manek <shaneal@greplin.com>
2011-04-04 20:32:31 -0700

    correct the copyright statement

--- src/main/java/com/greplin/bloomfilter/RepeatedMurmurHash.java
+++ src/main/java/com/greplin/bloomfilter/RepeatedMurmurHash.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 The Lucene Interval Field Authors.
+ * Copyright 2010 The Greplin Bloom Filter Authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
